d3f07be4c1c4a2d33592e4d459ea204f79c062c3
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-07-12 09:58:52 -0500

    [feat] create react project (#1)
    * feat: create react project
    
    * feat: create components and pages folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import HelloWorld from './components/HelloWorld';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App-header">
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;

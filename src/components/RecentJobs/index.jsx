856a565a2a4dd27792e2443d48927910e1102838
Khriztian Moreno <khriztianmoreno@gmail.com>
2022-07-19 15:15:04 -0500

    feat: create job service and update component (#4)

--- src/components/RecentJobs/index.jsx
+++ src/components/RecentJobs/index.jsx
@@ -1,105 +1,52 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const RecentJobs = () => (
-  <section>
-    <div className="container">
-      <header className="section-header">
-        <span>Latest</span>
-        <h2>Recent jobs</h2>
-      </header>
+import { getJobs } from '../../services/jobs'
 
-      <div className="row item-blocks-connected">
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-google.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Senior front-end developer</h4>
-                <h5>Google</h5>
+const RecentJobs = () => {
+  const [jobs, setJobs] = useState(getJobs());
+
+  return (
+    <section>
+      <div className="container">
+        <header className="section-header">
+          <span>Latest</span>
+          <h2>Recent jobs</h2>
+        </header>
+
+        <div className="row item-blocks-connected">
+          {
+            jobs.map(job => (
+              <div className="col-xs-12" key={job.id}>
+                <Link className="item-block" to={`/jobs/detail/${job.id}`}>
+                  <header>
+                    <img src={job.image} alt={job.title} />
+                    <div className="hgroup">
+                      <h4>{job.title}</h4>
+                      <h5>{job.company}</h5>
+                    </div>
+                    <div className="header-meta">
+                      <span className="location">{job.location}</span>
+                      <span className={`label ${job.className}`}>{job.type}</span>
+                    </div>
+                  </header>
+                </Link>
               </div>
-              <div className="header-meta">
-                <span className="location">Menlo park, CA</span>
-                <span className="label label-success">Full-time</span>
-              </div>
-            </header>
-          </a>
+            ))
+          }
         </div>
 
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-linkedin.png" alt="" />
-              <div className="hgroup">
-                <h4>Software Engineer (Entry or Senior)</h4>
-                <h5>Linkedin</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Livermore, CA</span>
-                <span className="label label-warning">Part-time</span>
-              </div>
-            </header>
-          </a>
-        </div>
+        <br />
 
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-envato.png" alt="" />
-              <div className="hgroup">
-                <h4>Full Stack Web Developer</h4>
-                <h5>Envato</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">San Francisco, CA</span>
-                <span className="label label-info">Freelance</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="./img/logo-facebook.png" alt="" />
-              <div className="hgroup">
-                <h4>Web Applications Developer</h4>
-                <h5>Facebook</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Lexington, MA</span>
-                <span className="label label-danger">Internship</span>
-              </div>
-            </header>
-          </a>
-        </div>
-
-        <div className="col-xs-12">
-          <a className="item-block" href="/">
-            <header>
-              <img src="/img/logo-microsoft.jpg" alt="" />
-              <div className="hgroup">
-                <h4>Sr. SQL Server Developer</h4>
-                <h5>Microsoft</h5>
-              </div>
-              <div className="header-meta">
-                <span className="location">Palo Alto, CA</span>
-                <span className="label label-success">Remote</span>
-              </div>
-            </header>
-          </a>
-        </div>
+        <br />
+        <p className="text-center">
+          <Link className="btn btn-info" to="/jobs">
+            Browse all jobs
+          </Link>
+        </p>
       </div>
-
-      <br />
-
-      <br />
-      <p className="text-center">
-        <Link className="btn btn-info" to="/jobs">
-          Browse all jobs
-        </Link>
-      </p>
-    </div>
-  </section>
-);
+    </section>
+  )
+};
 
 export default RecentJobs;
